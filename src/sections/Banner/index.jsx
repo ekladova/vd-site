63cd540e32dd0d095c5940d3a25fab4ef8ba050b
Kate Kladova <ek.kladova@gmail.com>
2020-07-19 22:49:26 +0100

    links

--- src/sections/Banner/index.jsx
+++ src/sections/Banner/index.jsx
@@ -5,6 +5,24 @@ const Banner = () => {
     <>
       <div className="banner">
         <h1 className="h1-header">Visual Studio Submission</h1>
+        <div>
+          <h3 className="h3-header">
+            <a
+              target="_blank"
+              href="https://giacomosorbi.github.io/ekladova-module-ii/"
+            >
+              Website based on the mock-ups
+            </a>
+          </h3>
+          <h3 className="h3-header">
+            <a
+              target="_blank"
+              href="https://github.com/ekladova/vd-site/tree/master/src/images"
+            >
+              A GitHub link for of all images used in this submission
+            </a>
+          </h3>
+        </div>
       </div>
     </>
   );
