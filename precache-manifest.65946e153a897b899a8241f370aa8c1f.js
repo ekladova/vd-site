e509e12f5867fdd6d631c1c3b75cd5ea944bdf95
Kate Kladova <ek.kladova@gmail.com>
2020-07-19 17:00:39 +0100

    Updates

---
+++ precache-manifest.65946e153a897b899a8241f370aa8c1f.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f51f3eb127ff67eb7146d967f88e01d2",
+    "url": "/vd-site/index.html"
+  },
+  {
+    "revision": "12c46142b4c842fbc018",
+    "url": "/vd-site/static/css/main.ab33f723.chunk.css"
+  },
+  {
+    "revision": "22bfe66e767f1d2ea674",
+    "url": "/vd-site/static/js/2.12058494.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vd-site/static/js/2.12058494.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12c46142b4c842fbc018",
+    "url": "/vd-site/static/js/main.756958fe.chunk.js"
+  },
+  {
+    "revision": "81535fc3a7bc445b7bd1",
+    "url": "/vd-site/static/js/runtime-main.f31a521e.js"
+  },
+  {
+    "revision": "c90039f26cf7d3abf5e0d531fd0cf589",
+    "url": "/vd-site/static/media/INSTApng.c90039f2.png"
+  },
+  {
+    "revision": "f5a8eeb16caeb791163daa3ce661507d",
+    "url": "/vd-site/static/media/about.f5a8eeb1.png"
+  },
+  {
+    "revision": "8757b81956530c5e0f62b2cc8bc89214",
+    "url": "/vd-site/static/media/banner-noname.8757b819.jpg"
+  },
+  {
+    "revision": "c19c7c2b38055cac37d6b9ba29feea85",
+    "url": "/vd-site/static/media/bannertree.c19c7c2b.jpg"
+  },
+  {
+    "revision": "3afa65b520249bd26e9b43dc346c559f",
+    "url": "/vd-site/static/media/facebook-original.3afa65b5.png"
+  },
+  {
+    "revision": "f0d0a6146aa55526cf2b03e819c3f188",
+    "url": "/vd-site/static/media/game.f0d0a614.png"
+  },
+  {
+    "revision": "01f035714fd575dbe20fb37b94dd67d5",
+    "url": "/vd-site/static/media/home.01f03571.jpg"
+  },
+  {
+    "revision": "dbcfafd22ce779d5bf141b45371dc378",
+    "url": "/vd-site/static/media/insta-original.dbcfafd2.png"
+  },
+  {
+    "revision": "65a4a011ed75f352a170420618fb3ab8",
+    "url": "/vd-site/static/media/pen-o.65a4a011.png"
+  },
+  {
+    "revision": "f58232419b69ce953c45de1f57fa72a6",
+    "url": "/vd-site/static/media/pen.f5823241.jpg"
+  },
+  {
+    "revision": "e09cfb939a0392b9cccfe3e6a2eb6bfb",
+    "url": "/vd-site/static/media/stroke.e09cfb93.svg"
+  },
+  {
+    "revision": "df2957cc727e0cd833b09a0ee369a4c7",
+    "url": "/vd-site/static/media/tree1.df2957cc.png"
+  },
+  {
+    "revision": "bfaf12ca82cd6dedcc482402d6a2e2a2",
+    "url": "/vd-site/static/media/woman-original.bfaf12ca.jpg"
+  },
+  {
+    "revision": "8021d91f8a0024988a93cb0a528188d5",
+    "url": "/vd-site/static/media/woman.8021d91f.jpg"
+  }
+]);
